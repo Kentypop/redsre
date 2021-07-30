6989d3a9bb6fa85a7cbd4cfa1bf00ee28a87687f
kentypop <Levimcc7@hotmail.com>
2021-07-30 21:41:49 +0900

    corey pt10 done

--- info/urls.py
+++ info/urls.py
@@ -2,14 +2,18 @@ from django.urls import path
 from . import views
 from .views import (PostListView, 
 	PostDetailView, 
-	PostCreateView
+	PostCreateView,
+    PostUpdateView,
+    PostDeleteView
 	)
 
 urlpatterns = [
     path('', views.PostListView.as_view() , name='info-home'),
     path('post/<int:pk>/', PostDetailView.as_view() , name='post-detail'),
     path('post/new/', PostCreateView.as_view() , name='post-create'),
-    path('about/', views.about, name='info-about'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view() , name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view() , name='post-delete'),
+    path('about/', views.about, name='info-about'), 
     path('hometry/', views.hometry, name='info-hometry'),
     path('price/', views.price, name='info-price'),
     path('pricenew/', views.pricenew, name='info-pricenew'),
--- info/views.py
+++ info/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 from .models import Post, Price
 from .forms import PriceForm
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
 	ListView, 
 	DetailView,
 	CreateView,
+	UpdateView,
+	DeleteView
 )	
 
 poststry= [
@@ -55,13 +58,39 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
 	model= Post	
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
 	model= Post	
 	fields= ['title', 'content']
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model= Post	
+	fields= ['title', 'content'] 
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)	
+
+	def test_func(self):
+		#Get the post that we currentl;y trying to update
+		post= self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model= Post	
+	success_url= '/'
+
+	def test_func(self):
+		#Get the post that we currentl;y trying to update
+		post= self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False						
 
 def hometry(request):
 	context= {
